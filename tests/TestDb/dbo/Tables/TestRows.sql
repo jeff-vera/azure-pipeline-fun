5f26c09a4ea35b1805324ac00f978dd267afaefd
Jeff Vera <jeff.vera@gmail.com>
2018-12-17 22:36:47 -0500

    trying

--- tests/TestDb/dbo/Tables/TestRows.sql
+++ tests/TestDb/dbo/Tables/TestRows.sql
@@ -1,11 +1,14 @@
 ﻿CREATE TABLE [dbo].[TestRows] (
-    [Id]               INT              NULL,
+    [Id]               INT              IDENTITY (1, 1) NOT NULL,
     [SomeText]         VARCHAR (50)     NOT NULL,
     [SomeBit]          BIT              NOT NULL,
     [SomeDate]         DATE             NOT NULL,
     [SomeDateTime]     DATETIME         NOT NULL,
     [SomeGUID]         UNIQUEIDENTIFIER NOT NULL,
     [SomeMoney]        MONEY            NOT NULL,
-    [SomeNullableText] VARCHAR (50)     NULL
+    [SomeNullableText] VARCHAR (50)     NULL,
+    CONSTRAINT [PK_TestRows] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
